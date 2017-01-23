45fd5563021a86e53c9c909d3d1c1276197cd6de
jgellatly5 <jgellatly5@gmail.com>
2017-01-23 14:59:26 -0800

    first commit
    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(function() {
+  if (HousesCollection.find().count() === 0) {
+  	var houses = [{
+  		name: 'Stephan',
+  		plants: [{
+  			color: 'red',
+  			instructions: '3 pots/week'
+  		}, {
+  			color: 'white',
+  			instructions: 'keep humid'
+  		}]
+  	}];
+
+  	while (houses.length > 0) {
+  		HousesCollection.insert(houses.pop());
+  	}
+  	console.log('Added fixtures');
+  }
+});
