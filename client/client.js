ac11d4f95977239252323be19e784823e3279fda
jgellatly5 <jgellatly5@gmail.com>
2017-01-24 11:42:23 -0800

    removing plants
    removing plants

--- client/client.js
+++ client/client.js
@@ -107,6 +107,15 @@ updateLocalHouse = function(id, modifier) {
 };
 
 Template.plantFieldset.events({
+	'click button.removePlant': function(evt) {
+		evt.preventDefault();
+		var index = evt.target.getAttribute('data-index');
+		var plants = Template.parentData(1).plants;
+		plants.splice(index, 1);
+		var modifier = {$set: {'plants': plants}};
+
+		updateLocalHouse(Session.get('selectedHouseId'), modifier);
+	},
 	'keyup input.color, keyup input.instructions': function(evt) {
 		evt.preventDefault();
 		var index = evt.target.getAttribute('data-index');
